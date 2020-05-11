9757bcb9df69c0af5951cc5916060b8eea65a8ea
Maxim Baykevich <max.baikevich@gmail.com>
2020-05-11 12:36:47 +0300

    mobile

---
+++ source/js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.slider-footer').slick({
+        centerMode:true,
+        centerPadding: '20px',
+        infinite: false,
+        edgeFriction: '0.5'
+      });
+  });
+  $(document).ready(function(){
+    $('.bunner-slider').slick({
+        centerMode:true,
+        centerPadding: '0',
+        infinite: false,
+        edgeFriction: '0.5'
+    });
+  });
